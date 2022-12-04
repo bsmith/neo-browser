72f05848eb4d45fc982d6e4cab11f8df01528196
Benjamin Smith <bsmith@cabbage.org.uk>
2022-12-04 15:39:21 +0000

    Better sortedBy

--- src/components/ObjectTable.js
+++ src/components/ObjectTable.js
@@ -1,13 +1,27 @@
 import ObjectRow from "./ObjectRow";
 
+let sortedBy = (array, keyFunc) => {
+    /* the map created a new array that is then sorted in-place */
+    /* 'decorate-sort-undecorate': https://en.wikipedia.org/wiki/Schwartzian_transform */
+    const newArray =
+        array
+        .map(e => [keyFunc(e), e])
+        .sort(([k1, _1], [k2, _2]) => {
+            if (k1 < k2) return -1;
+            if (k2 > k1) return 1;
+            return 0;
+        })
+        .map(([_, e]) => e);
+    return newArray;
+}
+
 const ObjectTable = ({objects}) => {
     if (!(objects && "map" in objects))
         return <p>Error: { JSON.stringify(objects) }</p>;
 
     const fields = ["name", "ca_date", "ca_dist", "vrel", "diameter", "magnitude", "hazardous"];
 
-    let sortedObjects = [...objects];
-    sortedObjects.sort((a, b) => a.close_approach_data[0].epoch_date_close_approach - b.close_approach_data[0].epoch_date_close_approach);
+    const sortedObjects = sortedBy(objects, object => object.close_approach_data[0].epoch_date_close_approach)
 
     const rowItems = sortedObjects.map(object => <ObjectRow key={object.id} fields={fields} object={object} />)
 
